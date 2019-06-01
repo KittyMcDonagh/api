1213f5309fbe4a572c1f930979ca7ebee95c0f7d
Kitty McDonagh <kittymcdonagh@gmail.com>
2019-06-01 20:45:53 +0000

    getting control over our data

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-var xhr = new XMLHttpRequest();
-var data;
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://swapi.co/api/");
-xhr.send();
+    xhr.open("GET", "https://swapi.co/api/");
+    xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-    console.log("data = ")
-    console.log(data);
-    console.log("jsonData = ")
-    console.log(jsonData);
-}
-
-xhr.onreadystatechange = function() {
-    console.log("readyState = "+this.readyState+" status = "+this.status)
-    if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText))          // This passes the data out to the setData function, which will store it in 'data'
-        
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
     };
 }
 
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+getData(printDataToConsole);
