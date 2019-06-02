3750699e9ccb683068f18022b1807864844810ca
Kitty McDonagh <kittymcdonagh@gmail.com>
2019-06-02 16:14:48 +0000

    tableheaders

--- js/main.js
+++ js/main.js
@@ -13,15 +13,29 @@ function getData(type, cb) {
     xhr.send();
 }
 
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+    
+    return `<tr>${tableHeaders}</tr>`;
+}
+
 function writeToDocument(type) {
     var el = document.getElementById("data");         // create an element for the data
     el.innerHTML = "";                                // clear the inner element. Each time you click on an object it will clear and display
                                                       // rather than adding to the end
     getData(type, function(data) {
         data = data.results;
+        
+        var tableHeaders = getTableHeaders(data[0]);   // create table headers
 
         data.forEach(function(item) {
-            el.innerHTML += "<p>" + item.name + "</p>";              // display the data in a par element for clarity
+           
+            // el.innerHTML += "<p>" + item.name + "</p>";              // display the data in a par element for clarity
         });
+        
+        el.innerHTML = `<tr>${tableHeaders}</tr>`;
     });
 }
