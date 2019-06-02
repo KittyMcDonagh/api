641e9ebf6db730fd08cab08f67da5af7edbd6a98
Kitty McDonagh <kittymcdonagh@gmail.com>
2019-06-02 15:20:44 +0000

    getting data onto the page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-function getData(cb) {
-    var xhr = new XMLHttpRequest();
+const baseURL = "https://swapi.co/api/";            // create constant for the base URL we're using
 
-    xhr.open("GET", "https://swapi.co/api/");
-    xhr.send();
+function getData(type, cb) {                    // pass in type (people, vehicles, films, etc) to getData function
+    var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");              // pass in base URL + type + '/'
+    xhr.send();
 }
 
-function printDataToConsole(data) {
-    console.log(data);
+function writeToDocument(type) {              // parameter "type" comes from api - people, films, vehicles, etc
+    getData(type, function(data) {            // calling an anonymous function
+        document.getElementById("data").innerHTML = data;
+    });
 }
-
-getData(printDataToConsole);
